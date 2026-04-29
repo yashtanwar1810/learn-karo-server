8d8b4621aeac32215bfe406d0430013b8f2dde7d
yash tanwar <yashtanwar810@gmail.com>
2026-04-29 23:40:17 +0530

    minor bug fix

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 import { env } from "../config/env.config.js";
 
 export const generateAccessToken = (id) => {
-    return jwt.sign({ id }, env.ACCESS_TOKEN_SECRET, {
+    return jwt.sign({ id }, env.JWT_SECRET, {
         expiresIn: "15m",
     });
 };
 
 export const generateRefreshToken = (id) => {
-    return jwt.sign({ id }, env.REFRESH_TOKEN_SECRET, {
+    return jwt.sign({ id }, env.JWT_SECRET, {
         expiresIn: "7d",
     });
 };
