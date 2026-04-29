45ad27ebc337e6ea4b89fd3a84af531ea9c5419f
yash tanwar <yashtanwar810@gmail.com>
2026-04-29 23:23:56 +0530

    minor fix

--- src/app.js
+++ src/app.js
@@ -13,9 +13,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    origin: env.ALLOWED_ORIGINS,
+    credentials: true
+}))
 
 // api routes
-app.get("/", (req, res) => res.json({message: "connected to backend!"}))
+app.get("/", (req, res) => res.json({ message: "connected to backend!" }))
 
 app.use("/api/v1/auth", authRouter)
--- src/config/env.config.js
+++ src/config/env.config.js
@@ -7,4 +7,6 @@ export const env = {
     JWT_SECRET: process.env.JWT_SECRET || "your_jwt_secret_key",
     MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/learnify",
     NODE_ENV: process.env.NODE_ENV || "development",
+
+    ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(",") : ["http://localhost:3000"],
 }
